a7836b109305da29d690bccd724e453ef9394fb6
leejiwon1202 <ccap122@skuniv.ac.kr>
2023-05-22 10:08:36 +0900

    230522 프로젝트 틀 생성

---
+++ app/src/main/java/com/example/driver_drowsiness_detection_app/DrowsyDetectActivity.java
@@ -0,0 +1,14 @@
+package com.example.driver_drowsiness_detection_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrowsyDetectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drowsy_detect);
+    }
+}
--- app/src/main/java/com/example/driver_drowsiness_detection_app/MainActivity.java
+++ app/src/main/java/com/example/driver_drowsiness_detection_app/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.driver_drowsiness_detection_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this, DrowsyDetectActivity.class);
+            startActivity(i);
+        });
+
+        Button logBtn = findViewById(R.id.logBtn);
+        logBtn.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this, LogActivity.class);
+            startActivity(i);
+        });
+
+        Button settingBtn = findViewById(R.id.settingBtn);
+        settingBtn.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this, SettingActivity.class);
+            startActivity(i);
+        });
     }
 }
