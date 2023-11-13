252908e2b0d0f42ded0f16eaf67b3e901e529d6d
Jiwon <ccap122@skuniv.ac.kr>
2023-11-13 23:14:40 +0900

    23113 수정코드

--- WEB/app.py
+++ WEB/app.py
@@ -1,30 +1,53 @@
-from flask import Flask,render_template,Response
-import cv2  
+from threading import Lock
+from flask import Flask, render_template, session
+from flask_socketio import SocketIO, emit
+import requests
 
-app=Flask(__name__)
-camera=cv2.VideoCapture(0)
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = None
 
-def generate_frames():
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+url = 'https://api.coinbase.com/v2/prices/btc-usd/spot'
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
     while True:
-        ## read the camera frame
-        success,frame=camera.read()
-        if not success:
-            break
-        else:
-            frame = cv2.flip(frame, 1)
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
-
-        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+        socketio.sleep(3)
+        count += 1
+        price = ((requests.get(url)).json())['data']['amount']
+        socketio.emit('my_response',
+                      {'data': 'Bitcoin current price (USD): ' + price, 'count': count})
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('indexT.html', async_mode=socketio.async_mode)
 
-@app.route('/video')
-def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+@socketio.event
+def my_event(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
 
-if __name__=="__main__":
-    app.run(debug=True)
+# Receive the test request from client and send back a test response
+@socketio.on('test_message')
+def handle_message(data):
+    print('received message: ' + str(data))
+    emit('test_response', {'data': 'Test response sent'})
+
+@socketio.event
+def connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+if __name__ == '__main__':
+    socketio.run(app)
--- WEB/stream.py
+++ WEB/stream.py
@@ -1,75 +1,139 @@
-from flask import Flask,render_template,Response, jsonify
+import requests
 import cv2 
-import torch
 import numpy as np
+import time
+import math
+
 from resnet3D import resnet3D50
 import torch.nn as nn
+import torch
+
+from flask import Flask,render_template,Response, session
+from flask_socketio import SocketIO, emit
+
+import mediapipe as mp
+
+
+# 1)
+camera=cv2.VideoCapture(0)
 
 STATIC_FOLDER = 'templates/assets'
-app = Flask(__name__,
-            static_folder=STATIC_FOLDER)
-camera=cv2.VideoCapture(0)
-value = "Wait.."
+app = Flask(__name__, static_folder=STATIC_FOLDER)
+async_mode = None
+socketio = SocketIO(app, async_mode=async_mode)
 
+
+# 2)
+def getUserState(model, input):
+    input = torch.Tensor((input)).unsqueeze(0)
+    result = model(input)[0].detach().cpu().numpy()
+    print(result) 
+
+    if result[1] >= 2:
+        return ("Drowsy!!!", (0, 0, 255))
+    else:
+        return ("Normal", (255, 0, 0))
+
+
+# 3)
+def calc_ear(p_list):
+    a_x = (p_list[0].x - p_list[3].x) ** 2 * 1000
+    a_y = (p_list[0].y - p_list[3].y) ** 2 * 1000
+    a = a_x + a_y
+            
+    b_x = (p_list[1].x - p_list[5].x) ** 2 * 1000
+    b_y = (p_list[1].y - p_list[5].y) ** 2 * 1000
+    b = b_x + b_y
+            
+    c_x = (p_list[2].x - p_list[4].x) ** 2 * 1000
+    c_y = (p_list[2].y - p_list[4].y) ** 2 * 1000
+    c = c_x + c_y
+    
+    return (b + c) / (2 * a)
+
+
+# 3)
 def generate_frames():
-    n = 0
-    value = "Wait.."
+    model_path = './work_model_29.pth'
+    model = resnet3D50(non_local=True)
+    model.fc = nn.Linear(2048,2)
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
     data = np.ones(shape=(3, 150, 320, 240), dtype=np.int8)
 
-    device = 'cpu'
+    mp_face_mesh = mp.solutions.face_mesh
 
-    model_path = './work_model_29.pth'
-    model = resnet3D50(non_local=True).to(device)
-    model.fc = nn.Linear(2048,2).to(device)
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    cnt_frame = 0
+    text_color = (255, 0, 0)
+    user_state = "Wait.."
+    
+    eye_state = "Opened"
+    list_index = [359, 387, 385, 362, 380, 373, 130, 160, 158, 133, 153, 144]
+    r_mar = 0
+    l_mar = 0
+    area_Saved = False
+    time_close = time.time()
 
-    while True:
-        success,frame=camera.read()
-        n += 1
-        
-        if not success:
-            break
-        else:
-            roi = frame[:, 140:500].copy()
-            img = cv2.resize(roi, (240, 320))
+    with mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5) as face_mesh:
+        while True:
+            success,frame=camera.read()
+            
+            if not success: break
+            else:
+                # 1) EAR
+                results = face_mesh.process(frame)
+                if results.multi_face_landmarks:
+                    list_coord = []
+                    for i in list_index:
+                        list_coord.append(results.multi_face_landmarks[0].landmark[i])
+                    l_ear = calc_ear(list_coord[:6])
+                    r_ear = calc_ear(list_coord[6:])
 
-            data[:, :-1, :, :] = data[:, 1:, :, :]
+                    if cnt_frame < 100 and not area_Saved:
+                        r_mar += r_ear; l_mar += l_ear
+                    elif not area_Saved:
+                        area_Saved = True
+                        r_mar /= 100;   l_mar /= 100
+                        print(r_mar, l_mar)
+                    elif (r_ear < r_mar * 0.8) and (l_ear < l_mar * 0.8):
+                        if eye_state == "Opened":
+                            time_close = time.time()
+                        if time.time() - time_close >= 4:
+                            socketio.emit('response', {'text': "warning"})
+                            socketio.sleep(1)
+                        eye_state = "Closed"
+                    else:
+                        eye_state = "Opened"
 
-            data[0, -1, :, :] = img[:,:,0]
-            data[1, -1, :, :] = img[:,:,1]
-            data[2, -1, :, :] = img[:,:,2]
 
-            if n == 150:
-                input = torch.Tensor(data).unsqueeze(0).to(device)
-                result = model(input)[0].detach().cpu().numpy() 
-                print(result)
-                
-                if result[1] >= 2: value = "Drowsy!!!"
-                else: value = "Normal"
+                # 2) 모델
+                roi = frame[:, 140:500].copy()
+                img = cv2.resize(roi, (240, 320))
 
-                n = 0
+                data[:, :-1, :, :] = data[:, 1:, :, :]
+                data[0, -1, :, :] = img[:,:,0]
+                data[1, -1, :, :] = img[:,:,1]
+                data[2, -1, :, :] = img[:,:,2]
 
-            frame = cv2.flip(frame, 1)
-            cv2.putText(frame, value, (30, 50),
-                        cv2.FONT_ITALIC, 1, (255, 0, 0), 2)
+                cnt_frame += 1
+                if cnt_frame == 150:
+                    user_state, text_color = getUserState(model, data)
+                    cnt_frame = 0
 
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
 
-        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                # 3) WEB에 전송하기 위한 frame 설정
+                frame = cv2.flip(frame, 1)
+                cv2.putText(frame, user_state, (30, 50), cv2.FONT_ITALIC, 1, text_color, 2)
+                _, buffer=cv2.imencode('.jpg',frame)
+                frame=buffer.tobytes()
+            yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
-    return render_template('index2.html', value=value)
+    return render_template('index.html')
 
 @app.route('/video')
 def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/hello', methods=["POST"])
-def hello():
-    data = {'value': 'hello'}
-    return jsonify(data)
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
     app.run(debug=True)
